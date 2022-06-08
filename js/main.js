f186b3d5fa19058d5cb7d03ebff0b45a72519e3d
Daniil <95589264+racoon-typing@users.noreply.github.com>
2022-06-08 18:20:27 +0300

    Нужно больше функций (#3)
    * добавляет наставника
    
    * Добавляет скрипт
    
    * на индекс
    
    * ghjuhfvvs
    
    * исправляет функцию
    
    * вывод в консоль
    
    * изменяет js файл
    
    * удаляет выводв консоль
    
    * результат в консоли
    
    * удаляет коммент
    
    * исправляет ошибки
    
    * удаляет выводв консоль

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function getRandomInt(min, max) {
+  if (max <= min) {
+    throw new Error('Измените значение max. Оно должно быть больше значения min');
+  }
+
+  if (max < 0, min < 0) {
+    throw new Error('Значение min и max должно быть не отрицательным');
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
+getRandomInt(0, 9);
+
+
+function getLengthComment(line, length) {
+  const lineLength = line.length;
+  const lengthComment = (lineLength> length) ? false : true;
+  return lengthComment;
+}
+
+getLengthComment(5, 10);
