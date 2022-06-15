53ca5a23a44dc1d5bd6397837ae68e6a26146265
Daniil <eo18622@yandex.ru>
2022-06-15 16:36:45 +0300

    создает массив

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+const NAME = [
+  'Олег',
+  'Миша',
+  'Никита',
+  'Вероника',
+  'Андрей',
+  'Петр',
+];
+
+const DESCRIPTIONS = [
+  'Море',
+  'Вода',
+  'Зелье',
+  'Набережная',
+  'Кот',
+  'Лучший момент',
+];
+
+const MESSAGE = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+
 function getRandomPositiveInteger (a, b) {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
@@ -5,11 +33,25 @@ function getRandomPositiveInteger (a, b) {
   return Math.floor(result);
 }
 
-getRandomPositiveInteger(0, 9);
+// function checkStringLength (string, length) {
+//   return string.length <= length;
+// }
 
-
-function checkStringLength (string, length) {
-  return string.length <= length;
+function getRandomArrayElement (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
 }
 
-checkStringLength(16, 10);
+const createPhoto = function () {
+  return {
+    id: getRandomPositiveInteger (1, 25),
+    url: `photos/${getRandomPositiveInteger (1, 25)}.jpg`,
+    description: getRandomArrayElement(DESCRIPTIONS),
+    likes: getRandomPositiveInteger (15, 200),
+    comments: ['id: ' + getRandomPositiveInteger (0, 200),
+      'avatar: ' + `img/avatar-${getRandomPositiveInteger (1, 6)}.svg`,
+      'message: ' + getRandomArrayElement(MESSAGE),
+      'name: ' + getRandomArrayElement(NAME)],
+  };
+};
+
+createPhoto();
