5cf695e7f499c4537ec94d6c7fef67e3876bfd32
Daniil <eo18622@yandex.ru>
2022-06-15 15:42:12 +0300

    получает функции

--- js/main.js
+++ js/main.js
@@ -1,22 +1,15 @@
-function getRandomInt(min, max) {
-  if (max <= min) {
-    throw new Error('Измените значение max. Оно должно быть больше значения min');
-  }
-
-  if (max < 0 || min < 0) {
-    throw new Error('Значение min и max должно быть не отрицательным');
-  }
-
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-getRandomInt(0, 9);
+getRandomPositiveInteger(0, 9);
 
 
-function getLengthComment(line, length) {
-  return line.length <= length;
+function checkStringLength (string, length) {
+  return string.length <= length;
 }
 
-getLengthComment(5, 10);
+checkStringLength(16, 10);
