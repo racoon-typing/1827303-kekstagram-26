8d349ad7ffcf7c52e3e5e64f763aafa9109f54e4
Daniil <95589264+racoon-typing@users.noreply.github.com>
2022-06-18 20:34:29 +0300

    Модуляция (#5)

---
+++ js/util.js
@@ -0,0 +1,12 @@
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function getRandomArrayElement (elements) {
+  return elements[getRandomPositiveInteger(0, elements.length - 1)];
+}
+
+export {getRandomArrayElement, getRandomPositiveInteger};
