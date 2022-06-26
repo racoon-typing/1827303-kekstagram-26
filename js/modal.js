338d7f70de2575b8b74fdd181bd82c641548f8fb
Daniil <eo18622@yandex.ru>
2022-06-26 21:20:08 +0300

    находим элемент

--- js/modal.js
+++ js/modal.js
@@ -1,2 +1,7 @@
 const modalWindow = document.querySelector('.big-picture');
-modalWindow.classList.remove(hidden);
+modalWindow.classList.remove('hidden');
+
+const bigImgWrapper = modalWindow.querySelector('.big-picture__img');
+const bigImg = bigImgWrapper.children;
+
+console.log(bigImg);
