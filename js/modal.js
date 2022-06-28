4e4980fb4082fedaeec54dba9744a4558d3e6464
Daniil <eo18622@yandex.ru>
2022-06-28 15:51:16 +0300

    добавляет поиск

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,24 @@
+import {simylarData} from './data.js';
+
 const modalWindow = document.querySelector('.big-picture');
 modalWindow.classList.remove('hidden');
 
 const bigImgWrapper = modalWindow.querySelector('.big-picture__img');
-const bigImg = bigImgWrapper.children;
 
-console.log(bigImg);
+const simylarModalPhoto = simylarData();
+const modalList = document.createDocumentFragment();
+
+simylarModalPhoto.forEach(({url, likes, comments}) => {
+  const bigImg = bigImgWrapper.children[0];
+  bigImg.src = url;
+
+
+
+  // const simylarElement = simylarPhotoTempalate.cloneNode(true);
+  // simylarElement.querySelector('.picture__img').src = url;
+  // simylarElement.querySelector('.picture__likes').textContent = likes;
+  // simylarElement.querySelector('.picture__comments').textContent = comments;
+
+  modalList.appendChild(simylarElement);
+});
+
