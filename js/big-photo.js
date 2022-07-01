c90bb9892ee2e85feaf6932bb43cb6484344c014
Daniil <eo18622@yandex.ru>
2022-07-01 17:00:57 +0300

    вставляет описание

--- js/big-photo.js
+++ js/big-photo.js
@@ -2,7 +2,7 @@ import {simylarData} from './data.js';
 
 const modalWindow = document.querySelector('.big-picture');
 const tegBody = document.querySelector('body');
-
+const commentDescription = document.querySelector('.social__caption');
 
 const buttonClose = document.querySelector('#picture-cancel');
 buttonClose.addEventListener('click', () => {
@@ -25,16 +25,20 @@ const simylarNewComment = simylarData();
 
 const addCommentList = document.createDocumentFragment();
 
-simylarNewComment.forEach(({comments}) => {
+simylarNewComment.forEach(({comments, description}) => {
   const simylarComment = tempalteCommentItem.cloneNode(true);
   simylarComment.querySelector('.social__picture').src = comments.avatar;
   simylarComment.querySelector('.social__picture').alt = comments.name;
   simylarComment.querySelector('.social__text').textContent = comments.message;
+  commentDescription.textContent = description;
 
   addCommentList.appendChild(simylarComment);
 });
 
 commentList.appendChild(addCommentList);
+
+
+
 
 
 
