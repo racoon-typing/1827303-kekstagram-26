0055d65b3ebea96c29078cba0ffdd2bc73b62715
Daniil <eo18622@yandex.ru>
2022-07-02 11:07:43 +0300

    переносит в биг фото

--- js/big-photo.js
+++ js/big-photo.js
@@ -4,6 +4,34 @@ const modalWindow = document.querySelector('.big-picture');
 const tegBody = document.querySelector('body');
 const commentDescription = document.querySelector('.social__caption');
 
+
+const bigPhoto = document.querySelector('.big-picture__img');
+const commentCount = document.querySelector('.comments-count');
+const likesCount = document.querySelector('.likes-count');
+
+function openModalWindow () {
+  const likesCounter = document.querySelector('.social__comment-count');
+  likesCounter.classList.add('hidden');
+
+  const uploadComment = document.querySelector('.comments-loader');
+  uploadComment.classList.add('hidden');
+
+  const tegBody = document.querySelector('body');
+  tegBody.classList.add('modal-open');
+}
+
+openModalWindow ();
+
+  simylarElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    modalWindow.classList.remove('hidden');
+    bigPhoto.children[0].src = url;
+    commentCount.textContent = comments.length;
+    likesCount.textContent = likes;
+    openModalWindow();
+  });
+
 const buttonClose = document.querySelector('#picture-cancel');
 buttonClose.addEventListener('click', () => {
   modalWindow.classList.add('hidden');
@@ -15,6 +43,14 @@ document.addEventListener('keydown', (e) => {
     modalWindow.classList.add('hidden');
   }
 });
+
+if (modalWindow.classList.contains('hidden')) {
+  document.removeEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      modalWindow.classList.add('hidden');
+    }
+  });
+}
 
 const commentList = document.querySelector('.social__comments');
 const tempalteCommentItem = document.querySelector('#comment-item')
--- js/miniature.js
+++ js/miniature.js
@@ -5,22 +5,6 @@ const simylarPhotoTempalate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const modalWindow = document.querySelector('.big-picture');
-const bigPhoto = document.querySelector('.big-picture__img');
-const commentCount = document.querySelector('.comments-count');
-const likesCount = document.querySelector('.likes-count');
-
-function openModalWindow () {
-  const likesCounter = document.querySelector('.social__comment-count');
-  likesCounter.classList.add('hidden');
-
-  const uploadComment = document.querySelector('.comments-loader');
-  uploadComment.classList.add('hidden');
-
-  const tegBody = document.querySelector('body');
-  tegBody.classList.add('modal-open');
-}
-
 const simylarPhoto = simylarData();
 
 const miniatureList = document.createDocumentFragment();
@@ -30,16 +14,6 @@ simylarPhoto.forEach(({url, likes, comments}) => {
   simylarElement.querySelector('.picture__img').src = url;
   simylarElement.querySelector('.picture__likes').textContent = likes;
   simylarElement.querySelector('.picture__comments').textContent = comments.length;
-
-  simylarElement.addEventListener('click', (evt) => {
-    evt.preventDefault();
-
-    modalWindow.classList.remove('hidden');
-    bigPhoto.children[0].src = url;
-    commentCount.textContent = comments.length;
-    likesCount.textContent = likes;
-    openModalWindow();
-  });
 
   miniatureList.appendChild(simylarElement);
 });
