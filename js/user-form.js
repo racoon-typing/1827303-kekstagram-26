f3f38a290b9c4a44aadac404044749671009cff8
Daniil <eo18622@yandex.ru>
2022-07-09 10:30:12 +0300

    загрузка изображения

--- js/user-form.js
+++ js/user-form.js
@@ -5,5 +5,10 @@ const form = document.querySelector('.img-upload__overlay');
 const uploadPhoto = document.querySelector('#upload-file');
 uploadPhoto.addEventListener('change', () => {
   console.log('Загрузилось');
+  document.body.classList.add('modal-open');
   form.classList.remove('hidden');
+
+  const fileImage = this.value;
+  console.log(fileImage);
+
 });
