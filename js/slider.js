3452a95c988df6ca2104b9e0d6f06f79fe1caa85
Daniil <eo18622@yandex.ru>
2022-07-12 10:25:22 +0300

    добавляет слайдер

---
+++ js/slider.js
@@ -0,0 +1,10 @@
+const slider = document.querySelector('.effect-level__value');
+
+noUiSlider.create(slider, {
+  start: [20, 80],
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 100
+  }
+});
