b20e4ca9253bf07d68d43c3b9c07b123d465834a
Daniil <eo18622@yandex.ru>
2022-07-17 13:59:44 +0300

    невидимый коммент

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
 import './miniature.js';
 import './big-photo.js';
-import './user-form.js';
-import './slider.js';
+// import './user-form.js';
--- js/miniature.js
+++ js/miniature.js
@@ -1,5 +1,5 @@
-import {simylarData} from './data.js';
-import {openModalWindow} from './big-photo.js';
+import { simylarData } from './data.js';
+import { openModalWindow } from './big-photo.js';
 
 const simylarListElement = document.querySelector('.pictures');
 const simylarPhotoTempalate = document.querySelector('#picture')
@@ -11,48 +11,29 @@ const similarPhoto = simylarData();
 const miniatureList = document.createDocumentFragment();
 
 const buttonLoadComment = document.querySelector('.comments-loader');
-let commentList = document.querySelector('.social__comments').children;
+const commentList = document.querySelector('.social__comments').children;
+const commentVisible = document.querySelector('.comments-visible');
 let invisibleNumber = 0;
 
-function getInvsibleComment () {
-  let j = 5;
+function getInvsibleComment() {
   for (let i = 0; i <= commentList.length; i++) {
 
     if (commentList.length > 5) {
-      commentList[j].classList.add('hidden');
-      j++;
+      for (let j = 5; j <= commentList.length; j++) {
+        commentList[j].classList.add('hidden');
+      }
     }
 
     if (commentList[i].classList.contains('hidden')) {
       invisibleNumber++;
       console.log(invisibleNumber);
     }
+
+    commentVisible.textContent = commentList.length - invisibleNumber;
   }
-
 }
 
-function getVisibleComment () {
-  buttonLoadComment.addEventListener('click', () => {
-
-    for (let i = 0; i <= commentList.length; i++) {
-      commentList[i].classList.remove('hidden');
-    }
-
-    if (commentList[i].classList.contains('hidden')) {
-      invisibleNumber++;
-      console.log(invisibleNumber);
-    }
-  });
-}
-
-getVisibleComment();
-
-const commentVisible = document.querySelector('.comments-visible');
-function commentCountVisible () {
-  commentVisible.textContent = commentList.length - invisibleNumber;
-}
-
-function createSimilarPhoto ({url, likes, comments, description}) {
+function createSimilarPhoto({ url, likes, comments, description }) {
   const simylarElement = simylarPhotoTempalate.cloneNode(true);
   simylarElement.querySelector('.picture__img').src = url;
   simylarElement.querySelector('.picture__likes').textContent = likes;
@@ -62,11 +43,21 @@ function createSimilarPhoto ({url, likes, comments, description}) {
     evt.preventDefault();
     openModalWindow(url, likes, comments, description);
     getInvsibleComment();
-    commentCountVisible();
   });
 
   miniatureList.appendChild(simylarElement);
 }
+
+// function getVisibleComment() {
+//   buttonLoadComment.addEventListener('click', () => {
+
+//     for (let i = 0; i <= commentList.length; i++) {
+//       commentList[i].classList.remove('hidden');
+//     }
+//   });
+// }
+
+// getVisibleComment();
 
 similarPhoto.forEach(createSimilarPhoto);
 
