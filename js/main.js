107445f3a4bf660d9d4f9b44f0d9b35aa48c17f2
Daniil <eo18622@yandex.ru>
2022-07-23 10:13:59 +0300

    добавляю папку

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import './miniature.js';
 import './big-photo.js';
 import './user-form.js';
+import './scale.js';
--- js/user-form.js
+++ js/user-form.js
@@ -52,42 +52,6 @@ function closenUploadPhoto() {
 
 closenUploadPhoto();
 
-// Изменеят значение Scale
-const buttonScaleSmaller = document.querySelector('.scale__control--smaller');
-const buttonScaleBigger = document.querySelector('.scale__control--bigger');
-const inputScale = document.querySelector('.scale__control--value');
-let scaleValueNumber = parseInt(inputScale.value, 10);
-const scaleContolStep = 25;
-
-function changeScaleValue() {
-  buttonScaleSmaller.addEventListener('click', () => {
-    if (scaleValueNumber === 25) {
-      return;
-    }
-
-    scaleValueNumber = scaleValueNumber - scaleContolStep;
-    inputScale.value = `${scaleValueNumber}%`;
-
-    uploadPhotoImg.style.transform = `scale(${scaleValueNumber / 100})`;
-    return inputScale.value;
-  });
-
-  buttonScaleBigger.addEventListener('click', () => {
-    if (scaleValueNumber === 100) {
-      return;
-    }
-
-    scaleValueNumber = scaleValueNumber + scaleContolStep;
-    inputScale.value = `${scaleValueNumber}%`;
-
-    uploadPhotoImg.style.transform = `scale(${scaleValueNumber / 100})`;
-    return inputScale.value;
-  });
-
-}
-
-changeScaleValue();
-
 // Добавляет эффект на фото
 const rangeSlider = document.querySelector('.effect-level');
 
