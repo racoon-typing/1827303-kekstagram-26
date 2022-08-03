6a081f0f1af21f2136d7fff1e247a397bdb05c3f
Daniil <95589264+racoon-typing@users.noreply.github.com>
2022-08-03 13:57:47 +0300

    Module12 task1 (#14)
    * добавляет наставника
    
    * удаляет hidden
    
    * получает данные
    
    * разделяет код
    
    * ошибка при загрузки формы
    
    * создает отлов ошибки
    
    * удаляет класс при нажатии на esc
    
    * удаляет файл
    
    * комментарий
    
    * отправка формы
    
    * разбор полетов
    
    * экспрорт клоуз
    
    * стирает форму
    
    * успешная отправка
    
    * саксест баттон
    
    * положительный ответ
    
    * by esc закрывает
    
    * закрытие всеми способами
    
    * кетч
    
    * закртие формы с ответом ошибка
    
    * ошибка
    
    * функция сброса
    
    * функция резет
    
    * ошибки
    
    * отправка запроса – успешно
    
    * esc
    
    * функция закрытия
    
    * кнопка закрытия окна с ошибкой
    
    * сообщение об успехе
    
    * закрытие модальног окна
    
    * закрытие модального окна
    
    * закрытие модальног окна
    
    * функция загурзки комментариев
    
    * коммент
    
    * функция отправки
    
    * функция отправки данных
    
    * доработка функционала
    
    * добавляет правильный стиль
    
    * работает ошибка
    
    * форма отправки
    
    * фильтрация
    
    * фильтр
    
    * первый фильтр
    
    * сортировка
    
    * 2 фильтра
    
    * фильтр по лайкам
    
    * сортировка
    
    * фильтр по числу лайков
    
    * сортировка слуйчаных
    
    * фильтрация
    
    * дебаунс
    
    * ссохраняет проект
    
    * cj

--- js/miniature.js
+++ js/miniature.js
@@ -1,5 +1,6 @@
 import { openModalWindow } from './big-photo.js';
 import { getInvsibleComment, countvisibleComment, getVisibleComment } from './comment.js';
+// import { debounce } from './util.js';
 
 const simylarListElement = document.querySelector('.pictures');
 const simylarPhotoTempalate = document.querySelector('#picture')
@@ -10,27 +11,95 @@ const simylarPhotoTempalate = document.querySelector('#picture')
 const miniatureList = document.createDocumentFragment();
 
 // Создание миниатюры
+const filterPhoto = document.querySelector('.img-filters');
+const formFilter = document.querySelector('.img-filters__form');
+
+function createSimilarPhoto({ url, likes, comments, description }) {
+  const simylarElement = simylarPhotoTempalate.cloneNode(true);
+  simylarElement.querySelector('.picture__img').src = url;
+  simylarElement.querySelector('.picture__likes').textContent = likes;
+  simylarElement.querySelector('.picture__comments').textContent = comments.length;
+
+  simylarElement.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openModalWindow(url, likes, comments, description);
+    getInvsibleComment();
+    countvisibleComment();
+    getVisibleComment();
+  });
+
+  miniatureList.appendChild(simylarElement);
+}
+
 const renderSimilarList = (similarPhoto) => {
-  function createSimilarPhoto({ url, likes, comments, description }) {
-    const simylarElement = simylarPhotoTempalate.cloneNode(true);
-    simylarElement.querySelector('.picture__img').src = url;
-    simylarElement.querySelector('.picture__likes').textContent = likes;
-    simylarElement.querySelector('.picture__comments').textContent = comments.length;
+  similarPhoto
+    .forEach(createSimilarPhoto);
 
-    simylarElement.addEventListener('click', (evt) => {
-      evt.preventDefault();
-      openModalWindow(url, likes, comments, description);
-      getInvsibleComment();
-      countvisibleComment();
-      getVisibleComment();
-    });
-
-    miniatureList.appendChild(simylarElement);
-  }
-
-  similarPhoto.forEach(createSimilarPhoto);
+  filterPhoto.classList.remove('img-filters--inactive');
 
   simylarListElement.appendChild(miniatureList);
+  comparePhoto(similarPhoto);
 };
 
-export {renderSimilarList};
+// const RERENDER_DELAY = 500;
+
+function appendPhoto () {
+  simylarListElement.appendChild(miniatureList);
+}
+
+// function setDebounce (cb) {
+//   cb();
+// }
+
+function comparePhoto(data) {
+  formFilter.addEventListener('click', (evt) => {
+    const photoElements = document.querySelectorAll('.picture');
+
+    if (evt.target.id === 'filter-default') {
+      for (let i = 0; i <= photoElements.length; i++) {
+        if (photoElements[i]) {
+          photoElements[i].remove();
+        }
+      }
+
+      data
+        .slice()
+        .forEach(createSimilarPhoto);
+
+      appendPhoto ();
+    }
+
+    if (evt.target.id === 'filter-random') {
+      for (let i = 0; i <= photoElements.length; i++) {
+        if (photoElements[i]) {
+          photoElements[i].remove();
+        }
+      }
+
+      data
+        .slice()
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 10)
+        .forEach(createSimilarPhoto);
+
+      appendPhoto ();
+    }
+
+    if (evt.target.id === 'filter-discussed') {
+      for (let i = 0; i <= photoElements.length; i++) {
+        if (photoElements[i]) {
+          photoElements[i].remove();
+        }
+      }
+
+      data
+        .slice()
+        .sort((a, b) => b.likes - a.likes)
+        .forEach(createSimilarPhoto);
+
+      appendPhoto ();
+    }
+  });
+}
+
+export { renderSimilarList };
